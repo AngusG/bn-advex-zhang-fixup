00354cd9fd8a0b78a7dce3f02255989742af10c2
AngusG <premiumdriedfoods@gmail.com>
2019-07-22 11:44:44 -0400

    update util

--- imagenet/util.py
+++ imagenet/util.py
@@ -14,6 +14,7 @@ import torch.nn.functional as F
 
 import numpy as np
 import torch
+from advertorch.attacks import LinfPGDAttack
 
 def mixup_data(x, y, alpha=1.0, use_cuda=True, per_sample=False):
 
@@ -182,6 +183,46 @@ class AverageMeter(object):
         self.sum += val * n
         self.count += n
         self.avg = self.sum / self.count
+'''
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self, name, fmt=':f'):
+        self.name = name
+        self.fmt = fmt
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+    def __str__(self):
+        fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
+        return fmtstr.format(**self.__dict__)
+'''
+
+class ProgressMeter(object):
+    def __init__(self, num_batches, *meters, prefix=""):
+        self.batch_fmtstr = self._get_batch_fmtstr(num_batches)
+        self.meters = meters
+        self.prefix = prefix
+
+    def print(self, batch):
+        entries = [self.prefix + self.batch_fmtstr.format(batch)]
+        entries += [str(meter) for meter in self.meters]
+        print('\t'.join(entries))
+
+    def _get_batch_fmtstr(self, num_batches):
+        num_digits = len(str(num_batches // 1))
+        fmt = '{:' + str(num_digits) + 'd}'
+        return '[' + fmt + '/' + fmt.format(num_batches) + ']'
 
 
 def accuracy(output, target, topk=(1,)):
@@ -199,3 +240,75 @@ def accuracy(output, target, topk=(1,)):
             correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
             res.append(correct_k.mul_(100.0 / batch_size))
         return res
+
+
+def validate_pgd(val_loader, model, criterion, ord='inf'):
+    batch_time = AverageMeter('Time', ':6.3f')
+    losses = AverageMeter('Loss', ':.4e')
+    top1 = AverageMeter('Acc@1', ':6.2f')
+    top5 = AverageMeter('Acc@5', ':6.2f')
+    progress = ProgressMeter(len(val_loader), batch_time, losses, top1, top5,
+                             prefix='Test: ')
+    # switch to evaluate mode
+    model.eval()
+    end = time.time()
+    adversary_linf = LinfPGDAttack(model, loss_fn=criterion, eps=0.01,
+                                   nb_iter=20, eps_iter=0.001, rand_init=False,
+                                   clip_min=-2.0, clip_max=2.0, targeted=False)
+    for i, (image, target) in enumerate(val_loader):
+        image = image.cuda(non_blocking=True)
+        target = target.cuda(non_blocking=True)
+        adv_untargeted = adversary_linf.perturb(image, target)
+
+        # compute output
+        output = model(adv_untargeted)
+        loss = criterion(output, target)
+        # measure accuracy and record loss
+        acc1, acc5 = accuracy(output, target, topk=(1, 5))
+        losses.update(loss.item(), image.size(0))
+        top1.update(acc1[0], image.size(0))
+        top5.update(acc5[0], image.size(0))
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+        if i % 1 == 0:
+            progress.print(i)
+    # TODO: this should also be done with the ProgressMeter
+    print(' * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}'
+          .format(top1=top1, top5=top5))
+    return (top1.avg, top5.avg)
+
+
+def validate(val_loader, model, criterion, do_awgn=False):
+    batch_time = AverageMeter('Time', ':6.3f')
+    losses = AverageMeter('Loss', ':.4e')
+    top1 = AverageMeter('Acc@1', ':6.2f')
+    top5 = AverageMeter('Acc@5', ':6.2f')
+    progress = ProgressMeter(len(val_loader), batch_time, losses, top1, top5,
+                             prefix='Test: ')
+    # switch to evaluate mode
+    model.eval()
+    with torch.no_grad():
+        end = time.time()
+        for i, (image, target) in enumerate(val_loader):
+            if do_awgn:
+                image += torch.randn_like(image) / 2
+            image = image.cuda(non_blocking=True)
+            target = target.cuda(non_blocking=True)
+            # compute output
+            output = model(image)
+            loss = criterion(output, target)
+            # measure accuracy and record loss
+            acc1, acc5 = accuracy(output, target, topk=(1, 5))
+            losses.update(loss.item(), image.size(0))
+            top1.update(acc1[0], image.size(0))
+            top5.update(acc5[0], image.size(0))
+            # measure elapsed time
+            batch_time.update(time.time() - end)
+            end = time.time()
+            if i % 1 == 0:
+                progress.print(i)
+        # TODO: this should also be done with the ProgressMeter
+        print(' * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}'
+              .format(top1=top1, top5=top5))
+    return (top1.avg, top5.avg)
